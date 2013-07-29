19d2e3bdd75b6dd7078c5f0228251c40ad843294
unscriptable <john@unscriptable.com>
2013-07-29 13:52:20 -0400

    Rename router -> _Router (will remove _).

--- architecture-examples/backbone/app/_Router.js
+++ architecture-examples/backbone/app/_Router.js
@@ -1,25 +1,27 @@
 'use strict';
 
 var Backbone = require('backbone');
-var todos = require('./todos/todos');
-var todoFilter = require('./todos/filter');
 
 // Todo Router
 // ----------
-var Workspace = Backbone.Router.extend({
+module.exports = Backbone.Router.extend({
+
+	initialize: function (options) {
+		this.todoFilter = options.todoFilter;
+		this.todos = options.todos;
+		Backbone.history.start();
+	},
+
 	routes: {
 		'*filter': 'setFilter'
 	},
 
 	setFilter: function (param) {
 		// Set the current filter to be used
-		todoFilter.filter = param || '';
+		this.todoFilter.filter = param || '';
 
 		// Trigger a collection filter event, causing hiding/unhiding
 		// of Todo view items
-		todos.trigger('filter');
+		this.todos.trigger('filter');
 	}
 });
-
-module.exports = new Workspace();
-Backbone.history.start();
